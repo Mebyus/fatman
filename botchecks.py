c31695ab8a2fee120c233a5b01d71880122210d2
Mebyus <31649307+Mebyus@users.noreply.github.com>
2019-05-17 19:13:20 +0400

    Updating to current state

--- botchecks.py
+++ botchecks.py
@@ -1,13 +1,27 @@
 from discord.ext import commands
 
 
+class AuthorNotInVoice(commands.CheckFailure):
+    def __init__(self, message = None):
+        super().__init__(message or 'You must be in a voice channel to use this command')
+
+
+class BotNotInVoice(commands.CheckFailure):
+    def __init__(self, message = None):
+        super().__init__(message or 'I need to be in a voice channel to perform this')
+
+
 def author_in_voice():
     def predicate(ctx: commands.context.Context) -> bool:
-        return bool(ctx.author.voice)
+        if not ctx.author.voice:
+            raise AuthorNotInVoice()
+        return True
     return commands.check(predicate)
 
 
 def bot_in_voice():
     def predicate(ctx: commands.context.Context) -> bool:
-        return bool(ctx.guild.voice_client)
+        if not ctx.guild.voice_client:
+            raise BotNotInVoice()
+        return True
     return commands.check(predicate)
--- music.py
+++ music.py
@@ -9,6 +9,7 @@ if not discord.opus.is_loaded():
     print("[FAILED] Opus library")
 
 _ffmpeg = r".\bin\ffmpeg.exe"
+_ffmpeg_before = r"-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
 _default_volume = 0.2
 
 
@@ -26,19 +27,17 @@ class PlayerState:
         self.client = client
         self.channel = channel
         self.volume = volume
+        self.stream = None
         self.songs = []  # basically a songs queue in a player
     
     def play_next(self, error):
         if self.songs:
-            new_entry = self.songs.pop(0)
-            print(new_entry.length)
-            audio_stream = discord.FFmpegPCMAudio(new_entry.audio_stream_url, executable = _ffmpeg, pipe = True, isurl = True)
-            audio_stream = discord.PCMVolumeTransformer(audio_stream, 0.2)
-            # self.channel.send("Now playing: " + new_entry.title)
+            next_song = self.songs.pop(0)
+            self.stream = discord.FFmpegPCMAudio(next_song.audio_stream_url, executable = _ffmpeg, pipe = True, isurl = True, before_options = _ffmpeg_before)
+            audio_stream = discord.PCMVolumeTransformer(self.stream, self.volume)
             self.client.play(audio_stream, after = self.play_next)
         else:
             pass
-            # await self.channel.send("Bad news! Queue has ended")
 
 
 class Music(commands.Cog):
@@ -49,7 +48,7 @@ class Music(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.states = {}  # dictionary of {guild_id: player_state} pairs 
+        self.players = {}  # dictionary of {guild_id: player_state} pairs
     
     # Commands section
 
@@ -62,32 +61,33 @@ class Music(commands.Cog):
         """
         if not ctx.author.voice:
             await ctx.send("Where are you? Join voice channel first.")
-        elif not ctx.guild.voice_client and ctx.guild.id not in self.states:
-            self.states[ctx.guild.id] = PlayerState(await ctx.author.voice.channel.connect(), ctx.channel)
+        elif not ctx.guild.voice_client and ctx.guild.id not in self.players:
+            self.players[ctx.guild.id] = PlayerState(await ctx.author.voice.channel.connect(), ctx.channel)
         elif not ctx.guild.voice_client:
-            self.states[ctx.guild.id].client = await ctx.author.voice.channel.connect()
-            self.states[ctx.guild.id].channel = ctx.channel
-        elif ctx.guild.id not in self.states:
-            self.states[ctx.guild.id] = PlayerState(ctx.guild.voice_client, ctx.channel)
+            self.players[ctx.guild.id].client = await ctx.author.voice.channel.connect()
+            self.players[ctx.guild.id].channel = ctx.channel
+        elif ctx.guild.id not in self.players:
+            self.players[ctx.guild.id] = PlayerState(ctx.guild.voice_client, ctx.channel)
         elif ctx.guild.voice_client.channel == ctx.author.voice.channel:
             await ctx.send("What's wrong with you!? I'm already here")
         else:
-            self.states[ctx.guild.id].channel = ctx.channel
+            self.players[ctx.guild.id].channel = ctx.channel
             await ctx.guild.voice_client.move_to(ctx.author.voice.channel)
     
     @commands.command(name = "quit", aliases = ["q", "d", "l", "disconnect", "leave"])
+    @botchecks.bot_in_voice()
+    @botchecks.author_in_voice()
     @commands.guild_only()
     async def quit(self, ctx):
-        if not ctx.author.voice:
-            pass
-        elif not ctx.guild.voice_client:
-            pass
-        elif ctx.guild.voice_client.channel != ctx.author.voice.channel:
+        client = ctx.guild.voice_client
+        if client.channel != ctx.author.voice.channel:
             pass
         else:
-            await ctx.guild.voice_client.disconnect()
+            await client.disconnect()
 
     @commands.command(name = "play", aliases = ["p"])
+    @botchecks.bot_in_voice()
+    @botchecks.author_in_voice()
     @commands.guild_only()
     async def play(self, ctx, *, url: str):
         """
@@ -95,83 +95,76 @@ class Music(commands.Cog):
         If there is a song currently in the queue, then it is queued until the next song is done playing.
         This command automatically searches as well from YouTube.
         """
-        if not ctx.author.voice:
-            pass
-        elif not ctx.guild.voice_client:
-            await ctx.send("Sorry, but I have nowhere to play")
-        elif ctx.guild.voice_client.channel != ctx.author.voice.channel:
-            pass
-        elif ctx.guild.voice_client.is_playing() or ctx.guild.voice_client.is_paused():
-            self.states[ctx.guild.id].songs.append(SongEntry(url))
-            await ctx.send("Your song has been added to queue")
+        client = ctx.guild.voice_client
+        player = self.players[ctx.guild.id]
+
+        if client.channel != ctx.author.voice.channel:
+            return
+
+        new_song = SongEntry(url)
+        if new_song.length <= 600:
+            player.songs.append(new_song)
         else:
-            new_entry = SongEntry(url)
-            print(new_entry.length)
-            audio_stream = discord.FFmpegPCMAudio(new_entry.audio_stream_url, executable = _ffmpeg, pipe = True, isurl = True, before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
-            audio_stream = discord.PCMVolumeTransformer(audio_stream, self.states[ctx.guild.id].volume)
-            await ctx.send("Now playing: " + new_entry.title)
-            ctx.guild.voice_client.play(audio_stream, after = self.states[ctx.guild.id].play_next)
+            await ctx.send("This song is too long. I will ignore it")
+            return
+        if not (client.is_playing() or client.is_paused()):
+            player.play_next(None)
 
     @commands.command(name = "skip", aliases = ["s", "n", "next"])
+    @botchecks.bot_in_voice()
+    @botchecks.author_in_voice()
     @commands.guild_only()
     async def skip(self, ctx):
-        if not ctx.author.voice:
+        client = ctx.guild.voice_client
+        if client.channel != ctx.author.voice.channel:
             pass
-        elif not ctx.guild.voice_client:
-            pass
-        elif ctx.guild.voice_client.channel != ctx.author.voice.channel:
-            pass
-        elif not ctx.guild.voice_client.is_playing():
+        elif not client.is_playing():
             pass
         else:
-            ctx.guild.voice_client.stop()
+            client.stop()
 
     @commands.command(name = "pause", aliases = ["w", "wait"])
-    @botchecks.author_in_voice()
     @botchecks.bot_in_voice()
+    @botchecks.author_in_voice()
     @commands.guild_only()
     async def pause(self, ctx):
-        if not ctx.author.voice:
+        client = ctx.guild.voice_client
+        if client.channel != ctx.author.voice.channel:
             pass
-        elif not ctx.guild.voice_client:
-            pass
-        elif ctx.guild.voice_client.channel != ctx.author.voice.channel:
-            pass
-        elif not ctx.guild.voice_client.is_playing():
+        elif not client.is_playing():
             pass
         else:
-            ctx.guild.voice_client.pause()
+            client.pause()
 
     @commands.command(name = "resume", aliases = ["r", "continue"])
+    @botchecks.bot_in_voice()
+    @botchecks.author_in_voice()
     @commands.guild_only()
     async def resume(self, ctx):
-        if not ctx.author.voice:
+        client = ctx.guild.voice_client
+        if client.channel != ctx.author.voice.channel:
             pass
-        elif not ctx.guild.voice_client:
-            pass
-        elif ctx.guild.voice_client.channel != ctx.author.voice.channel:
-            pass
-        elif not ctx.guild.voice_client.is_paused():
+        elif not client.is_paused():
             pass
         else:
-            ctx.guild.voice_client.resume()
+            client.resume()
 
     @commands.command(name = "volume", aliases = ["v"])
+    @botchecks.bot_in_voice()
+    @botchecks.author_in_voice()
     @commands.guild_only()
     async def volume(self, ctx, *, volume: str):
-        if not ctx.author.voice:
-            pass
-        elif not ctx.guild.voice_client:
-            pass
-        elif ctx.guild.voice_client.channel != ctx.author.voice.channel:
+        client = ctx.guild.voice_client
+        player = self.players[ctx.guild.id]
+        if client.channel != ctx.author.voice.channel:
             pass
         else:
             try:
                 new_volume = int(volume)
                 if 0 <= new_volume <= 100:
-                    self.states[ctx.guild.id].volume = new_volume / 100
+                    player.volume = new_volume / 100
                 else:
-                    self.states[ctx.guild.id].volume = _default_volume
+                    player.volume = _default_volume
                     await ctx.send("Value \"" + volume + "\" is out of scope. Must be between 1 and 100\n Volume is set to default = " + str(int(100 * _default_volume)))
             except ValueError:
                 await ctx.send("Unable to convert value \"" + volume + "\" to an integer")
@@ -189,31 +182,51 @@ class Music(commands.Cog):
     async def quit_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
             await ctx.send(error)
+        dm_channel = ctx.author.dm_channel or await ctx.author.create_dm()
+        if isinstance(error, (botchecks.AuthorNotInVoice, botchecks.BotNotInVoice)):
+            dm_channel.send(error)
 
     @play.error
     async def play_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
             await ctx.send(error)
+        dm_channel = ctx.author.dm_channel or await ctx.author.create_dm()
+        if isinstance(error, botchecks.AuthorNotInVoice):
+            await dm_channel.send(error)
+        if isinstance(error, botchecks.BotNotInVoice):
+            await ctx.send("Sorry, but I have nowhere to play")
 
     @skip.error
     async def stop_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
             await ctx.send(error)
+        dm_channel = ctx.author.dm_channel or await ctx.author.create_dm()
+        if isinstance(error, (botchecks.AuthorNotInVoice, botchecks.BotNotInVoice)):
+            await dm_channel.send(error)
 
     @pause.error
     async def pause_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
             await ctx.send(error)
+        dm_channel = ctx.author.dm_channel or await ctx.author.create_dm()
+        if isinstance(error, (botchecks.AuthorNotInVoice, botchecks.BotNotInVoice)):
+            await dm_channel.send(error)
 
     @resume.error
     async def resume_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
             await ctx.send(error)
+        dm_channel = ctx.author.dm_channel or await ctx.author.create_dm()
+        if isinstance(error, (botchecks.AuthorNotInVoice, botchecks.BotNotInVoice)):
+            await dm_channel.send(error)
 
     @volume.error
     async def volume_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
             await ctx.send(error)
+        dm_channel = ctx.author.dm_channel or await ctx.author.create_dm()
+        if isinstance(error, (botchecks.AuthorNotInVoice, botchecks.BotNotInVoice)):
+            await dm_channel.send(error)
 
 
 def setup(bot):
